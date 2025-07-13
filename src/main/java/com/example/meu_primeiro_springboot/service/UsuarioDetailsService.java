b0d1b6a79d6af50abeea8be9cdf22b7355bd3ac2
Davi Lemos da Silva <davilemosilva@hotmail.com>
2025-07-12 21:36:54 -0300

    confirm

--- src/main/java/com/example/meu_primeiro_springboot/service/UsuarioDetailsService.java
+++ src/main/java/com/example/meu_primeiro_springboot/service/UsuarioDetailsService.java
@@ -1,4 +1,34 @@
 package com.example.meu_primeiro_springboot.service;
 
-public class UsuarioDetailsService {
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.meu_primeiro_springboot.model.Usuario;
+import com.example.meu_primeiro_springboot.repository.UsuariosRepository;
+
+@Service
+public class UsuarioDetailsService implements UserDetailsService {
+
+    private final UsuariosRepository usuarioRepository;
+
+    public UsuarioDetailsService(UsuariosRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Usuario usuario = usuarioRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+
+        return User.builder()
+                .username(usuario.getUsername())
+                .password(usuario.getPassword())
+                .roles("USER")
+                .build();
+    }
+
 }
